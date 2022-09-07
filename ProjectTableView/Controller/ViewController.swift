b0788dd34cc4ef1d76db8ad659eae452b0070a7d
Abraham Vazquez <mabraham.vazquez@gmail.com>
2022-09-07 18:47:43 -0500

    [UPDATE] Actualizacion de clase

--- ProjectTableView/Controller/ViewController.swift
+++ ProjectTableView/Controller/ViewController.swift
@@ -1,9 +1,6 @@
-//
 //  ViewController.swift
 //  ProjectTableView
-//
 //  Created by Moisés Abraham Vázquez Pérez on 07/09/22.
-//
 
 import UIKit
 
@@ -15,6 +12,7 @@ class ViewController: UIViewController {
             self.vwContainer.backgroundColor = UIColor(red: 236, green: 240, blue: 241, alpha: 0.35)
         }
     }
+    
     @IBOutlet weak var vwImage: UIImageView!{
         didSet{
             self.vwImage.layer.cornerRadius = 50
@@ -26,7 +24,6 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
