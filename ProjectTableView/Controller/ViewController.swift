5c93cf2dea55cd54bf890462466957a8ae9a2c77
Semjase <gisela.semjase@gmail.com>
2022-09-12 19:08:17 -0500

    [UPDATE]  Actualización en la clase de Login * Se agregaron datos de usuario y pass * Se modifico la vista para mostrar puntos en pass * Se agregó delegado en el campo de texto * Se agrego el first responder al label de usuario * Se valida campos para navegar

--- ProjectTableView/Controller/ViewController.swift
+++ ProjectTableView/Controller/ViewController.swift
@@ -19,24 +19,81 @@ class ViewController: UIViewController {
         }
     }
     
-    @IBOutlet weak var lblUser: UITextField!
-    @IBOutlet weak var lblPsswrd: UITextField!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        print("ESTE CAMIBO LO HIZO MOY")
-        
+    @IBOutlet weak var lblUser: UITextField!{
+        didSet{ self.lblUser.delegate = self }
     }
     
+    @IBOutlet weak var lblPsswrd: UITextField!{
+        didSet{ self.lblPsswrd.delegate = self }
+    }
+    
+    
+    
+    //MARK: - D A T O S
+    let arrUsers: [String] = ["Luis_28", "AnaPata", "GisSemjase", "BioMoi"]
+    let arrPassW: [String] = ["neilgaiman300","nidodepatas","fall30", "placebomusic"]
+    
+    var MyUser: String = ""
+    var MyPass: String = ""
+    
+    
+    
+    //MARK: - L I F E · C Y C L E
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        view.addGestureRecognizer(tapGesture)
+    }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
+        self.lblUser.becomeFirstResponder()
+    }
+    
+    
+    //MARK: - F U N C T I O N S
+    @objc func hideKeyboard() {
+        view.endEditing(true)
+    }
+    
+    
+    func showAlert(WithTitle strTitle:String , AndMessage strMessage:String) {
+            let alert = UIAlertController(title: strTitle , message: strMessage, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Aceptar", style: .default , handler: nil))
+            alert.addAction(UIAlertAction(title: "Cancelar", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+    
+    
+    //MARK: - V A L I D A T I O N S
+    func validaCampoTx(EnMiBase usrs: [String], YDato nom: String) -> Bool {
+        if usrs.contains(nom){
+          return true
+        }else{
+          return false
+        }
+    }
+    
+    //MARK: - A C T I O N S
+    @IBAction func BtnInicio(_ sender: Any) {
+        MyPass = lblPsswrd.text ?? "No hay Pass"
+        MyUser = lblUser.text ?? "no hay user"
         
-        print("AÑADI UNA FUNCION DEL Ciclo de vida.")
+        if validaCampoTx(EnMiBase: arrUsers, YDato: MyUser) && validaCampoTx(EnMiBase: arrPassW, YDato: MyPass)
+        {
+            self.performSegue(withIdentifier: "VistaTabla", sender: self)
+        }else{
+            self.showAlert(WithTitle: "¡Alerta!", AndMessage: "Hay un error o eres un espia")
+        }
         
     }
 
-
 }
 
+//MARK: - EXTENSION - UI·TEXFIELD·DELEGATE
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.BtnInicio(self)
+        return true
+    }
+}
