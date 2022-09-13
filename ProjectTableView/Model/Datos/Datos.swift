a06b30279b458391315d018935db5f64043ca6f6
anayeli9 <anayeli.eel@gmail.com>
2022-09-12 19:42:34 -0500

    [UPDATE] Cell Country, Food, Music

--- ProjectTableView/Model/Datos/Datos.swift
+++ ProjectTableView/Model/Datos/Datos.swift
@@ -12,3 +12,20 @@ struct Datos {
     var datos2: String = ""
     var datos3: String = ""
 }
+class Food {
+    let arrFoodNames: [String] = ["EnsaladaConHongos","PechugaJardinera","Camarones","Sushi","PolloFrito","Salmon","Arrachera","PechugaYEnsalada","SushiSalmon","Albondigas"]
+
+    let arrDescFood: [String] = ["Muy Fresca","Se sirve con pan","Pidelos a la diabla","Solo o empanizado","Pidelo con papas tambien","Directamente desde Japón","De la res mas brava","Se vale repetir","Este es un clasico","Pidelas con salsa bolognesa"]
+}
+
+class Paises {
+    let arrCountryNames: [String] = ["Argentina", "Belgica", "Canada", "China", "Cuba", "Italia", "Japon", "Corea", "Mexico", "Venezuela" ,"Vaticano"]
+}
+
+class Music {
+    let arrArtistas: [String] = ["Los Acosta","The Weekend","Daft Punk","Deadmau5","Franz Ferdinand","Post Malone","Gorillaz","DaftPunk","The Strokes","Empire of the Sun"]
+
+    let arrAlbumName: [String] = ["CorazonesSolitarios","BlindingLights","Discovery","forLackOfABetterName","FranzFerdinand","HollywoodsBleeding","PlasticBeach","RAM","RoomOnFire","WalkingOnADream"]
+
+    let arrSongs: [String] = ["Como una Novela","Blinding Lights","One More Time","Gosht Stuff","This Fire","Enemies","Stylo","Instant Crush","Reptillia","Walking on a Dream","","","",""]
+}
---
+++ ProjectTableView/View/Cell/MusicCell/MusicCellTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MusicCellTableViewCell.swift
+//  ProjectTableView
+//
+//  Created by Anayeli Guerrero Pulido on 12/09/22.
+//
+
+import UIKit
+
+class MusicCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
