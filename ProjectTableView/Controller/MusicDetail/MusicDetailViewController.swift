06359e7239698d25c5e6ddacb9c697a0e9ad5bf7
Semjase <gisela.semjase@gmail.com>
2022-09-28 22:51:18 -0500

    ADD : Se agregó ViewControles con detalles de comida

---
+++ ProjectTableView/Controller/MusicDetail/MusicDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MusicDetailViewController.swift
+//  ProjectTableView
+//
+//  Created by Sem on 28/09/22.
+//
+
+import UIKit
+
+class MusicDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
